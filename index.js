1f9650e0e56d24469d86a6763a99665b1c251fd0
Kipronokirui <victorkipronokirui2@gmail.com>
2023-12-12 22:02:56 +0300

    Assignment Finished

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet > 2500){
+    return "No can do.";
+    console.log("No can do.")
+  }else if (feet <= 400){
+    let message = "This one is on me!"
+    console.log(message)
+    return(message)
+    
+  } else if(feet > 2000){
+    let charges = "I will gladly take your thirty bucks.";
+    console.log(charges)
+    return(charges)
+    
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city == "NYC"){
+    return "Ok, sounds good."
+  }else{
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip == "generous"){
+    return "Thank you so much."
+  }else if(tip != "generous"){
+    return "Thank you."
+  }else{
+    return "Bye."
+  }
 }
